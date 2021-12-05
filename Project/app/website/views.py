e025fb82ad53c48c69bdd4d188eda1c775bf2b0e
fgarada1 <foziea.g@gmail.com>
2021-12-04 18:31:52 -0600

    added search method for diseases

--- Project/app/website/views.py
+++ Project/app/website/views.py
@@ -21,3 +21,17 @@ def search(request):
       return render(request, 'search.html', {'diseases': map_of_diseases.keys(), 'symptoms': map_of_diseases.values()})
 
   return render(request, 'search.html', {'diseases': [], 'symptoms': []})
+
+def search(request):
+  if request.method == 'POST':
+      form = DiseasesForm(request.POST)
+      if form.is_valid():
+          search_term = form.cleaned_data['search']
+      main_dataset = pd.read_csv('../../../Research/ramarao2/dataset.csv')
+      for index, row in main_dataset.iterrows():
+        if index.str.contains(search_term):
+            return render(request, 'search.html', {'symptoms': row})
+        # if index equals the search term
+        # {‘symptoms’: list_goes_here}
+
+  return render(request, 'search.html', {'symptoms': []})
